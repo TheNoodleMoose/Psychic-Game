d3f4b0b0cd07943378ecd2d11b1034a75788e15f
TheNoodleMoose <cjh605@gmail.com>
2018-11-06 15:22:51 -0500

    almost finished, trying to figure out how to set randomLetter to a new letter after you win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,53 @@
 console.log("This Works!")
 
-//makes array of letters
+
 //setup a var that keeps track of wins and losses 
 //setup a var that stores the letters guessed already
 //when player wins make win counter go up, when they lose make loss counter go up
 
-var alphabet = ['a', 'b' , 'c' , 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+;
+
+var wins = 0;
+var losses = 1;
+var guesses = 9;
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(randomLetter);
-var guess = prompt("What Letter Am I Thinking Of?");
-var lower = guess.toLowerCase();
-if(lower === randomLetter) {
-    alert("That's It!");
-}
-else {
-    alert("That's Not It!");
+var playersGuess = [];
+
+if (guesses >= 1) {
+    document.onkeyup = function () {
+        console.log("Guesses equals " + guesses);
+        var playerChoice = event.key;
+        var lower = playerChoice.toLowerCase();
+        console.log(playerChoice);
+        console.log(randomLetter);
+        
+
+        if (lower === randomLetter) {
+            playersGuess = [];
+            alert("That's It!");
+            guesses = 9;
+            wins++;
+            document.getElementById("winsnumber").innerHTML = wins;
+            document.getElementById("guesses").innerHTML = guesses;
+            document.getElementById("guessedLetters").innerHTML = playersGuess;
+        }
+        else if(guesses >= 1 && lower !== randomLetter) {
+            alert("That's Not It!");
+            guesses--;
+            document.getElementById("guesses").innerHTML = guesses;
+            playersGuess.push(event.key)
+            console.log(playersGuess);
+            document.getElementById("guessedLetters").innerHTML = playersGuess;
+        } 
+        else if(guesses === 0) {
+            alert("You Lost!");
+            document.getElementById("losses").innerHTML = losses;
+            losses++;
+            guesses = 10;
+            playersGuess = [];
+        }
+    }
 }
 
