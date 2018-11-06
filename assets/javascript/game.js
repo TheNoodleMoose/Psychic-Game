73af73b5f23bec0cf3ad54151596de7071332760
TheNoodleMoose <cjh605@gmail.com>
2018-11-06 09:39:51 -0500

    adding initial files that makes the game work, but must apply it to html

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+console.log("This Works!")
+
+//makes array of letters
+//setup a var that keeps track of wins and losses 
+//setup a var that stores the letters guessed already
+//when player wins make win counter go up, when they lose make loss counter go up
+
+var alphabet = ['a', 'b' , 'c' , 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(randomLetter);
+var guess = prompt("What Letter Am I Thinking Of?");
+if(guess === randomLetter) {
+    alert("That's It!");
+}
+else {
+    alert("That's Not It!")
+}
